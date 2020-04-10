1a8e1dcb194a50ce227b8967474cb3d5a854ba61
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-10 17:57:21 -0300

    listas e tuplas

---
+++ aula5.py
@@ -0,0 +1,36 @@
+lista = [12, 6, 1 ,3, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+nova_lista  = lista_animal * 3
+
+tuple = (1,10,12,14)
+print(tuple[3])
+print(len(tuple))
+print(len(lista_animal))
+
+
+# nova_lista.sort()
+# lista.sort()
+# lista_animal.sort()
+# print(lista_animal)
+# print(lista)
+# lista_animal.reverse()
+# print(lista_animal)
+
+# print(sum(lista))
+# print(max(lista))
+# print(min(lista))
+# print(min(lista_animal))
+# print(max(lista_animal))
+
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('não existe um lobo na lista')
+#     lista_animal.append('lobo')
+#     print('lobo incluido na lista')
+#     print(lista_animal)
+#
+# lista_animal.pop()
+# lista_animal.pop(0)
+# lista_animal.remove('elefante')
+# print(lista_animal)
