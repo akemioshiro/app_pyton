ce582534849a2e4b568af870c17f909fc4600296
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-10 19:42:31 -0300

    funções e classes

--- aula7_televisao.py
+++ aula7_televisao.py
@@ -1,6 +1,7 @@
 class Televisao:
     def __init__(self):
         self.ligada = False
+        self.canal = 5
 
     def power(self):
         if self.ligada:
@@ -8,7 +9,22 @@ class Televisao:
         else:
             self.ligada = True
 
+    def aumenta_canal(self):
+        if self.ligada:
+            self.canal +=1
+
+    def diminui_canal(self):
+        if self.ligada:
+            self.canal -=1
+
 televisao = Televisao()
 print((televisao.ligada))
 televisao.power()
 print(televisao.ligada)
+print(televisao.canal)
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print(televisao.canal)
+televisao.diminui_canal()
+print(televisao.canal)
