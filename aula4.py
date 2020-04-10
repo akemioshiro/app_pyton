a1e806cb00ee4f82a58bce5e7ce1ae00397581d6
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-10 17:26:44 -0300

    laços de repetição

--- aula4.py
+++ aula4.py
@@ -1,9 +1,13 @@
+nota = int(input('Entre com a nota: '))
+while nota > 10:
+    nota = int(input('Entre com a nota: '))
+print(nota)
 
-a = 0
-while a < 10:
-    print(a)
-    a+=1
 
+# a = 0
+# while a < 10:
+#     print(a)
+#     a+=1
 
 # a = int(input('Digite um valor: '))
 # for num in range(a):
