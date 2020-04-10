dccf9a0249b3f1cd721130722723f02f4a28ac77
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-10 17:24:23 -0300

    laços de repetição

--- aula4.py
+++ aula4.py
@@ -1,3 +1,31 @@
 
-for x in range(100):
-    print(x)
+a = 0
+while a < 10:
+    print(a)
+    a+=1
+
+
+# a = int(input('Digite um valor: '))
+# for num in range(a):
+#     div = 0
+#     for x in range(1, num+1):
+#         resto = num % x
+#         if resto == 0:
+#              div +=1
+#     if div==2 :
+#          print(num)
+
+
+# a = int(input('Entre com o número: '))
+# div = 0
+#
+# for x in range(1, a+1):
+#     resto = a % x
+#     print(x,resto)
+#     if resto == 0:
+#         div +=1
+#
+# if div==2:
+#     print('Número {} é primo' .format(a))
+# else:
+#     print('Número {} não é primo'.format(a))
