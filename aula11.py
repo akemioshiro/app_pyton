0b39ac3d0e375f48dfe01be71a1d9db7b445ede9
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-10 21:59:34 -0300

    try/except

--- aula11.py
+++ aula11.py
@@ -1,6 +1,8 @@
 lista = [1, 10]
 
 try:
+    arquivo = open('teste.txt', 'r')
+    texto = arquivo.read()
     divisao = 10 / 1
     numero = lista [1]
     x = a
@@ -10,3 +12,8 @@ except IndexError:
     print('Erro ao acessar um indice invalido da lista')
 except BaseException as ex:
     print('Erro desconhecido: {} '.format(ex))
+else:
+    print('executa quando não ocorre exceção')
+finally:
+    print('sempre executa')
+    arquivo.close()
